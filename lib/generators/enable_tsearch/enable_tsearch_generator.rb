bb2420c4d44e3f8db70a168bafbb00dbd4a61f96
Robert Mathews <rob@justsoftwareconsulting.com>
2013-06-29 14:12:40 -0400

    apparently :context is a special word in erb. use search_context instead

--- lib/generators/enable_tsearch/enable_tsearch_generator.rb
+++ lib/generators/enable_tsearch/enable_tsearch_generator.rb
@@ -6,7 +6,7 @@ class EnableTsearchGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
   source_root File.expand_path('../templates', __FILE__)  
   argument :table_name, :type => :string
-  argument :context, :type=>:string
+  argument :search_context, :type=>:string
   argument :columns, :type => :array, :banner => "col1 col2 col3 ..."
    
   
@@ -16,22 +16,22 @@ class EnableTsearchGenerator < Rails::Generators::Base
   end
     
   def create_context
-    tmp = "  search_context [#{columns.join(',')}], :context=>:#{context}\n" unless columns.empty?
-    tmp = "  search_context :a_method_that_returns_the_search_terms, :context=>:#{context}\n" if columns.empty?
+    tmp = "  search_context [:#{columns.join(',:')}], :search_context=>:#{search_context}\n" unless columns.empty?
+    tmp = "  search_context :a_method_that_returns_the_search_terms, :search_context=>:#{search_context}\n" if columns.empty?
     inject_into_file "app/models/#{table_name.singularize}.rb", tmp , :after => /class #{table_name.singularize.camelize} < .*\n/
     migrate_if_needed("add_column_migration.rb","db/migrate/add_#{column_name}_to_#{table_name}.rb")
-    migrate_if_needed("add_trigger_migration.rb", "db/migrate/add_#{context}_trigger_to_#{table_name}.rb")
-    migrate_if_needed("add_tsearch_index_migration.rb", "db/migrate/add_#{context}_tsearch_index_to_#{table_name}.rb")
+    migrate_if_needed("add_trigger_migration.rb", "db/migrate/add_#{search_context}_trigger_to_#{table_name}.rb")
+    migrate_if_needed("add_tsearch_index_migration.rb", "db/migrate/add_#{search_context}_tsearch_index_to_#{table_name}.rb")
     migrate_if_needed("populate_vector_migration.rb", "db/migrate/populate_#{column_name}_on_#{table_name}.rb")
-    migrate_if_needed("populate_search_terms_migration.rb", "db/migrate/populate_#{context}_from_#{table_name}.rb")
+    migrate_if_needed("populate_search_terms_migration.rb", "db/migrate/populate_#{search_context}_from_#{table_name}.rb")
   end
   
   protected
   def column_name
-    "#{context}_vector"
+    "#{search_context}_vector"
   end
   def search_config_name
-     "#{context}_search_config"
+     "#{search_context}_search_config"
    end
   def index_name
     "idx_#{column_name}_on_#{table_name}"    
--- lib/generators/enable_tsearch/templates/populate_search_terms_migration.rb
+++ lib/generators/enable_tsearch/templates/populate_search_terms_migration.rb
@@ -1,9 +1,8 @@
 class <%= migration_class_name %> < ActiveRecord::Migration
   def up
-      execute(%Q{drop table if exists term_vectors})
       execute <<-EOS
-       -- DELETE from <%=context %>;
-       INSERT into <%=context %>(name, count,created_at, updated_at)
+       -- DELETE from <%=search_context %>;
+       INSERT into <%=search_context %>(name, count,created_at, updated_at)
        SELECT word, ndoc as count, now() as created_at, now() as updated_at
        FROM ts_stat(
        'SELECT to_tsvector(''simple'',
@@ -16,7 +15,7 @@ class <%= migration_class_name %> < ActiveRecord::Migration
       EOS
   end
   def down
-    execute %Q{DELETE from <%=context %>}
+    execute %Q{DELETE from <%=search_context %>}
   end
 end
 
