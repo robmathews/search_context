d830617d646825733adf47fbec1fb20609bed885
Robert Mathews <rob@justsoftwareconsulting.com>
2013-06-29 14:16:57 -0400

    remove tmp directory

--- spec/dummy/spec/tmp/db/migrate/20130625214627_add_trigram_index_to_foos.rb
+++
@@ -1,8 +0,0 @@
-class AddTrigramIndexToFoos < ActiveRecord::Migration
-  def up 
-    execute "CREATE INDEX idx_trgm_on_foos on foos USING gin (name gin_trgm_ops)"
-  end
-  def down
-    execute "DROP INDEX idx_trgm_on_foos"
-  end
-end
